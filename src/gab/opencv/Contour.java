6807ab8ff09f4f131adb20ae50677324ae76fc09
Greg Borenstein <greg.borenstein@gmail.com>
2014-11-02 10:18:34 -0500

    add comments to Contour class in response to confused email

--- src/gab/opencv/Contour.java
+++ src/gab/opencv/Contour.java
@@ -51,6 +51,14 @@ public class Contour {
 		}
 	}
 	
+	/**
+	 * Check if the Contour contains a given x-y point.
+	 * Particularly, useful for interaction via mouseX and mouseY.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return boolean 
+	 */
 	public boolean containsPoint(int x, int y){
 		Point p = new Point(x,y);
 		MatOfPoint2f m = new MatOfPoint2f(pointMat.toArray());
@@ -59,26 +67,50 @@ public class Contour {
 		return r == 1;
 	}
 	
-	// The polygonApproximationFactor is used to determine
-	// how strictly to follow a curvy polygon when converting
-	// it into a simpler polygon with getPolygonApproximation().
-	// For advanced use only. Set to a sane value by default.	
+	/**
+	 * The polygonApproximationFactor is used to determine
+	 * how strictly to follow a curvy polygon when converting
+	 * it into a simpler polygon with getPolygonApproximation().
+	 * For advanced use only. Set to a sane value by default.	
+	 * 
+	 * @param polygonApproximationFactor, a double
+	 */
 	public void setPolygonApproximationFactor(double polygonApproximationFactor){
 		this.polygonApproximationFactor = polygonApproximationFactor;
 	}
 	
+	/**
+	 * Access the current polygonApproximationFactor. The polygonApproximationFactor
+	 * is used to determine how strictly to follow a curvy polygon when converting
+	 * it into a simpler polygon with getPolygonApproximation().
+	 * 
+	 * @return polygonApproximationFactor, a double
+	 */
 	public double getPolygonApproximationFactor(){
 		return polygonApproximationFactor;
 	}
 	
+	/**
+	 * Get a new Contour that results from calculating
+	 * the polygon approximation of the current Contour.
+	 * The tightness of the approximation is set by the polygonApproximationFactor,
+	 * See setPolygonApproximationFactor() and getPolygonApproximationFactor().
+	 * 
+	 * @return
+	 */
 	public Contour getPolygonApproximation(){
 		MatOfPoint2f approx = new MatOfPoint2f();
 	    Imgproc.approxPolyDP(new MatOfPoint2f(inputPoints), approx, polygonApproximationFactor, true);
 		return new Contour(parent, approx);
 	}
 	
+	/**
+	 * Calculate a convex hull from the current Contour.
+	 * Returns a new Contour representing the convex hull.
+	 * 
+	 * @return Contour
+	 */
 	public Contour getConvexHull(){
-		
 		  MatOfInt hull = new MatOfInt();
 		  MatOfPoint points = new MatOfPoint(pointMat);
 		  
@@ -96,6 +128,10 @@ public class Contour {
 		  return new Contour(parent, hullPoints);
 	}
 	
+	/**
+	 * Draw the Contour as a closed shape with one vertex per-point.
+	 * 
+	 */
 	public void draw(){
 		parent.beginShape();
 		for (PVector p : points) {
@@ -104,19 +140,38 @@ public class Contour {
 		parent.endShape(PConstants.CLOSE);
 
 	}
-	
+	/**
+	 * Get the points that make up the Contour.
+	 * 
+	 * @return ArrayList<PVector> points
+	 */
 	public ArrayList<PVector> getPoints(){
 		return points;
 	}
 	
+	/**
+	 * The number of points in the Contour.
+	 * 
+	 * @return int
+	 */
 	public int numPoints(){
 		return points.size();
 	}
 	
+	/**
+	 * Get the bounding box for the Contour.
+	 * 
+	 * @return A java.awt.Rectangle
+	 */
 	public Rectangle getBoundingBox(){
 		return boundingBox;
 	}
 	
+	/**
+	 * The area of the Contour's bounding box. In most cases, this is a good approximation for the Contour's area.
+	 * 
+	 * @return float area
+	 */
 	public float area(){
 		return (boundingBox.width * boundingBox.height);
 	}
